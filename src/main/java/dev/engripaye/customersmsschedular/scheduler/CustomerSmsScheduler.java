067491d1e600e6dbc4498fbdb937110fecace726
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-11-15 19:17:12 +0100

    backend code commit by ENGR. IPAYE

--- src/main/java/dev/engripaye/customersmsschedular/scheduler/CustomerSmsScheduler.java
+++ src/main/java/dev/engripaye/customersmsschedular/scheduler/CustomerSmsScheduler.java
@@ -6,6 +6,9 @@ import dev.engripaye.customersmsschedular.service.SmsService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Objects;
+
 @Component
 public class CustomerSmsScheduler {
 
@@ -21,7 +24,22 @@ public class CustomerSmsScheduler {
     @Scheduled(cron = "0 0 22 * * ?")
     public void sendDailySms(){
 
+    try {
+        List<List<Object>> customers = sheetService.getAllCustomers();
 
+        for (List<Object> customer : customers) {
+            if (customer.size() >= 2) { // Name & contact must exist
+                String name = customer.get(0).toString();
+                String phone = customer.get(1).toString();
+
+                String message = String.format("Hello" + name + ", thank you for shopping with us today! We appreciate your feedback. Hope to see you again on your next shopping.");
+                smsService.sendSms(phone, message);
+
+            }
+        }
+    }catch (Exception e){
+        System.err.println("Error sending Sms: " + e.getMessage());
+    }
 
     }
 }
